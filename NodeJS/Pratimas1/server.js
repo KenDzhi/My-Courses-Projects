70bb7725c51130b5a7873fd342db8eace5cb1b69
Mantas Bartkevičius <mantas.bartkevicius1@gmail.com>
2023-05-05 20:09:37 +0300

    First task with Node

--- NodeJS/Pratimas1/server.js
+++ NodeJS/Pratimas1/server.js
@@ -1,4 +1,19 @@
-const name = "Mantas";
-const nickname = "KenDzhi";
+// const name = "Mantas";
+// const nickname = "KenDzhi";
 
-console.log(`Hello from Node world. My name is ${name} but in online world I'm known as ${nickname}`);
+// console.log(`Hello from Node world. My name is ${name} but in online world I'm known as ${nickname}`);
+
+
+// ---------------- Uzduotis ---------------------
+
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+function randomNumberGenerator(min, max) {
+    const rng = Math.random() * (max - min) + min;
+    return rng
+  }
+
+console.log(randomNumberGenerator(Number(process.argv[2]), Number(process.argv[3])))
+
+// --------------------------------------------------
